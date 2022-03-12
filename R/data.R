6f0b5f1e0e6a23a7fd0edfaf2b526f4faa1dbf7a
Abdul M. Sulley <33542748+qwickmalik@users.noreply.github.com>
2022-03-11 20:12:52 -0500

    Version 0.8

--- R/data.R
+++ R/data.R
@@ -2,8 +2,8 @@
 #' @description Dummy PK dataset for a 2x2 crossover study involving THC. Study 
 #' subjects were randomized to receive two products, A and B in randomly assigned 
 #' sequences AB or BA. THC levels were measured in their blood immediately before 
-#' product consumption, and at 10 time points after consumption.
-#' @format A data frame with 264 rows and 9 variables:
+#' product consumption, and at 12 time points after consumption.
+#' @format A data frame with 288 rows and 9 variables:
 #' \describe{
 #'   \item{\code{SUBJECTNUM}}{integer Unique subject ID}
 #'   \item{\code{CONC}}{double Concentration of analyte (ng/mL)}
@@ -16,6 +16,22 @@
 #'   \item{\code{ANALYTEN}}{integer Numeric coding for analyte} 
 #'}
 "pkdata"
+
+#' @title Dummy PK Steady-state Dataset
+#' @description Dummy PK steady-state dataset for a single study participant. 
+#' @format A data frame with 49 rows and 9 variables:
+#' \describe{
+#'   \item{\code{SUBJECTNUM}}{integer Unique subject ID}
+#'   \item{\code{CONC}}{double Concentration of analyte (ng/mL)}
+#'   \item{\code{TIME}}{double Time (h)}
+#'   \item{\code{SEQ}}{character Treatment sequence ("AB" or "BA")}
+#'   \item{\code{PERIOD}}{integer Study period (1 or 2)}
+#'   \item{\code{TRT}}{character Treatment/Investigational Product received ("A" or "B")}
+#'   \item{\code{GROUPING}}{character Study group. Usually the same as TRT}
+#'   \item{\code{ANALYTE}}{character Analyte being measured}
+#'   \item{\code{ANALYTEN}}{integer Numeric coding for analyte} 
+#'}
+"pkssdata"
 
 #' @title Dummy Repeated Measures Dataset
 #' @description Dummy repeated measures dataset.
